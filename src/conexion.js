d60e86bbca45da0155d7626381e789d15c570d1b
hector <andresvicentehector@gmail.com>
2025-07-14 22:22:29 +0200

    local

--- src/conexion.js
+++ src/conexion.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const conexion = async () => {
-  //await mongoose.connect("mongodb://127.0.0.1:27017/NIMBUS");
-  await mongoose.connect("mongodb://mongo:3010/NIMBUS"); // esto para amazon
+  await mongoose.connect("mongodb://127.0.0.1:27017/NIMBUS");
+  //
+  // await mongoose.connect("mongodb://mongo:3010/NIMBUS"); // esto para amazon
   console.log(
     "connectado correctamente a la base de datos de NIMBUSWALLS/VUIT"
   );
